dd2cbfbe486d7fe41453079ad633fe06815cf6a7
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-09-04 19:14:43 -0400

    Update configuration_bert.py

--- src/transformers/models/bert/configuration_bert.py
+++ src/transformers/models/bert/configuration_bert.py
@@ -114,6 +114,8 @@ class BertConfig(PretrainedConfig):
         position_embedding_type="absolute",
         use_cache=True,
         classifier_dropout=None,
+        softmax_term = 5,
+        softmax_bw = 32,
         **kwargs,
     ):
         super().__init__(pad_token_id=pad_token_id, **kwargs)
@@ -132,7 +134,9 @@ class BertConfig(PretrainedConfig):
         self.layer_norm_eps = layer_norm_eps
         self.position_embedding_type = position_embedding_type
         self.use_cache = use_cache
-        self.classifier_dropout = classifier_dropout
+        self.classifier_dropout = classifier_dropout,
+        self.softmax_term = softmax_term
+        self.softmax_bw = softmax_bw
 
 
 class BertOnnxConfig(OnnxConfig):

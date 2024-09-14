cd80fb297c6cb2e87ea2d05fc8c902111f62f4c0
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-09-14 00:28:32 -0400

    Update modeling_gpt2.py

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -576,6 +576,8 @@ class GPT2MLP(nn.Module):
         #hidden_states = self.act(hidden_states)
         #hidden_states = torch.nn.functional.gelu(hidden_states)
         hidden_states = custom_int_gelu(hidden_states, 16, 3).to(hidden_states)
+        if torch.isnan(hidden_states).any():
+            print('custon gelu overflow', hidden_states.dtype)
         hidden_states = self.c_proj(hidden_states)
         hidden_states = self.dropout(hidden_states)
         return hidden_states
